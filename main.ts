58e7f2495ee313e8042fa665377238ff9a0ed6a9
VSCO-Slender <63726209+VSCO-Slender@users.noreply.github.com>
2020-04-15 09:53:04 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+. . . . . . 5 . 5 . . . . . . . 
+. . . . . f 5 5 5 f f . . . . . 
+. . . . f 1 5 6 5 1 e f . . . . 
+. . . f 1 e e e e e 1 e f . . . 
+. . . f e e f f f f e 1 f . . . 
+. . . f e f f d d f f e f . . . 
+. . f e f d f d d f d f e f . . 
+. . f e f d e d d e d f e f . . 
+. . f e e f d d d d f e e f . . 
+. f e e f 6 f f f f 6 f e e f . 
+. . f f d 6 6 6 6 6 6 d f f . . 
+. . f d d f 6 6 6 6 f d d f . . 
+. . . f f 6 6 6 6 6 6 f f . . . 
+. . . f 6 6 6 6 6 6 6 6 f . . . 
+. . . f f f f f f f f f f . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.Player)
