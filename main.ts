63e03b27f29cbb9e3b8eedaffab9079c9c0afd21
VSCO-Slender <63726209+VSCO-Slender@users.noreply.github.com>
2020-05-01 14:18:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -119,17 +119,18 @@ namespace myTiles {
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Object, function (sprite, otherSprite) {
     info.changeScoreBy(1)
-    music.baDing.play()
-    if (info.score() == 10) {
-        game.over(true)
-    }
+    pages.destroy()
+    generatepages()
     if (info.score() == 8) {
-        currentlevel += 1
-        level_2()
+        level2()
     }
-    pages.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
+    if (info.score() == 15) {
+        game.showLongText("CONGRADULATIONS!!", DialogLayout.Bottom)
+        game.over(true, effects.confetti)
+    }
 })
-function level_2 () {
+function level2 () {
+    game.showLongText("LEVEL 2", DialogLayout.Bottom)
     tiles.setTilemap(tiles.createTilemap(
             hex`1000100004040404050404040404040405040404040504040404050404040504040405040404040404040404040404040404040404040405040404040404040404040404040404040404040405040405040404040404040404050404040404040404040404050404040404050405040404040504040404050404040404040404040404040404040404040404040404040404040404040405040405040404040504040504050404040404040404040404040404040404050404040404050404040404040404040404040504040404040404050404040404050404040404040405040404040404040404040404050404040404050404050404040404040404040404040404`,
             img`
@@ -155,9 +156,80 @@ function level_2 () {
         ))
     mySprite2.follow(mySprite)
     scene.cameraFollowSprite(mySprite)
+    mySprite.setPosition(81, 59)
+    mySprite.setVelocity(30, 20)
+    mySprite2.setPosition(4, 3)
+    mySprite2.setVelocity(10, 10)
 }
-function level_1 () {
-    tiles.setTilemap(tiles.createTilemap(
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    if (info.life() == 0) {
+        game.over(false, effects.melt)
+    }
+})
+function generatepages () {
+    pages = sprites.create(img`
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 f f f f f f f f f f 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 f f f f f f f f f f 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 f f f f f f f f f f 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 f f f f f f f f f f 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+`, SpriteKind.Object)
+    pages.setPosition(Math.randomRange(155, 115), Math.randomRange(155, 115))
+}
+let pages: Sprite = null
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+. . . . . . 5 . 5 . . . . . . . 
+. . . . . f 5 5 5 f f . . . . . 
+. . . . f 1 5 6 5 1 e f . . . . 
+. . . f 1 e e e e e 1 e f . . . 
+. . . f e e f f f f e 1 f . . . 
+. . . f e f f d d f f e f . . . 
+. . f e f d f d d f d f e f . . 
+. . f e f d e d d e d f e f . . 
+. . f e e f d d d d f e e f . . 
+. f e e f 6 f f f f 6 f e e f . 
+. . f f d 6 6 6 6 6 6 d f f . . 
+. . f d d f 6 6 6 6 f d d f . . 
+. . . f f 6 6 6 6 6 6 f f . . . 
+. . . f 6 6 6 6 6 6 6 6 f . . . 
+. . . f f f f f f f f f f . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.Player)
+mySprite2 = sprites.create(img`
+. f f . . . 1 1 1 . . . f f . . 
+. . f f . . 1 1 1 . . f f . . . 
+. . . f f . 1 1 1 . f f . . . . 
+. . . . f f f 1 f f f . . . . . 
+. . . f f f 1 f 1 f f f . . . . 
+. . . f . f 1 f 1 f . f . . . . 
+. . . f . f 1 f 1 f . f . . . . 
+. . f f f f f 1 f f f f f . . . 
+. f f f . f f f f f . f f f . . 
+f f . f . f f f f f . f . f f . 
+f . . f . f f . f f . f . . f f 
+f . . 1 . f . . . f . 1 . . . f 
+. . . . . f . . . f . . . . . . 
+. . . . . f . . . f . . . . . . 
+. . . . f f . . . f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Enemy)
+controller.moveSprite(mySprite, 100, 100)
+scene.cameraFollowSprite(mySprite)
+tiles.setTilemap(tiles.createTilemap(
             hex`1000100000000000000003000000000000000000000300000000000000000000000300000000000300000000000300000000000000000000000300000000000000000003000003000000000000000300000000000000000000000300000000000000000003000000000000000300000000000300000000000000000000000003000000000000000300000000000000000000000000000000000000030000030000030000000300000000000000000000000000000000000000030000000000000300000000000000000000000003000000000000000003000000000000000000000000000300000000000300000000000003000000000003000000000000030000000000`,
             img`
 . . . . . . . . . . . . . . . . 
@@ -180,228 +252,18 @@ function level_1 () {
             [myTiles.tile0,myTiles.tile1,sprites.castle.tilePath1,sprites.castle.saplingPine,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5],
             TileScale.Sixteen
         ))
-    pages = sprites.create(img`
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-`, SpriteKind.Object)
-    pages2 = sprites.create(img`
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-`, SpriteKind.Object)
-    pages3 = sprites.create(img`
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 f f f f f f f f f f 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
-`, SpriteKind.Object)
-    pages4 = sprites.create(img`
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-`, SpriteKind.Object)
-    pages5 = sprites.create(img`
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-`, SpriteKind.Object)
-    pages6 = sprites.create(img`
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 f f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 f f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 f f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 f f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-`, SpriteKind.Object)
-    pages7 = sprites.create(img`
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-`, SpriteKind.Object)
-    pages8 = sprites.create(img`
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 f f f f f f f f f f 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-`, SpriteKind.Object)
-}
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-    if (info.life() == 0) {
-        game.over(false)
-    }
-})
-let pages8: Sprite = null
-let pages7: Sprite = null
-let pages6: Sprite = null
-let pages5: Sprite = null
-let pages4: Sprite = null
-let pages3: Sprite = null
-let pages2: Sprite = null
-let pages: Sprite = null
-let mySprite2: Sprite = null
-let mySprite: Sprite = null
-let currentlevel = 0
-if (currentlevel == 0) {
-    mySprite = sprites.create(img`
-. . . . . . 5 . 5 . . . . . . . 
-. . . . . f 5 5 5 f f . . . . . 
-. . . . f 1 5 6 5 1 e f . . . . 
-. . . f 1 e e e e e 1 e f . . . 
-. . . f e e f f f f e 1 f . . . 
-. . . f e f f d d f f e f . . . 
-. . f e f d f d d f d f e f . . 
-. . f e f d e d d e d f e f . . 
-. . f e e f d d d d f e e f . . 
-. f e e f 6 f f f f 6 f e e f . 
-. . f f d 6 6 6 6 6 6 d f f . . 
-. . f d d f 6 6 6 6 f d d f . . 
-. . . f f 6 6 6 6 6 6 f f . . . 
-. . . f 6 6 6 6 6 6 6 6 f . . . 
-. . . f f f f f f f f f f . . . 
-. . . . . f f . . f f . . . . . 
-`, SpriteKind.Player)
-    mySprite2 = sprites.create(img`
-. f f . . . 1 1 1 . . . f f . . 
-. . f f . . 1 1 1 . . f f . . . 
-. . . f f . 1 1 1 . f f . . . . 
-. . . . f f f 1 f f f . . . . . 
-. . . f f f 1 f 1 f f f . . . . 
-. . . f . f 1 f 1 f . f . . . . 
-. . . f . f 1 f 1 f . f . . . . 
-. . f f f f f 1 f f f f f . . . 
-. f f f . f f f f f . f f f . . 
-f f . f . f f f f f . f . f f . 
-f . . f . f f . f f . f . . f f 
-f . . 1 . f . . . f . 1 . . . f 
-. . . . . f . . . f . . . . . . 
-. . . . . f . . . f . . . . . . 
-. . . . f f . . . f f . . . . . 
-. . . . . . . . . . . . . . . . 
-`, SpriteKind.Enemy)
-    controller.moveSprite(mySprite, 100, 100)
-    scene.cameraFollowSprite(mySprite)
-    mySprite.setPosition(74, 54)
-    mySprite.setKind(SpriteKind.Player)
-    info.setLife(3)
-    info.setScore(0)
-    controller.moveSprite(mySprite)
-    mySprite2.setPosition(11, 9)
-    mySprite2.follow(mySprite, 30)
-    mySprite2.setVelocity(10, 10)
-    mySprite2.setKind(SpriteKind.Enemy)
-    mySprite.setPosition(74, 111)
-    mySprite.setVelocity(30, 20)
-    level_1()
-}
+mySprite.setPosition(74, 54)
+mySprite.setKind(SpriteKind.Player)
+mySprite2.setPosition(11, 9)
+mySprite2.follow(mySprite, 30)
+mySprite2.setVelocity(10, 10)
+mySprite2.setKind(SpriteKind.Enemy)
+mySprite.setPosition(74, 111)
+mySprite.setVelocity(30, 20)
+info.setLife(3)
+info.setScore(0)
+generatepages()
 forever(function () {
     pages.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
-    pages2.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
-    pages3.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
-    pages4.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
-    pages5.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
-    pages6.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
-    pages7.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
-    pages8.setPosition(Math.randomRange(30, 250), Math.randomRange(30, 250))
     pause(5000)
 })
